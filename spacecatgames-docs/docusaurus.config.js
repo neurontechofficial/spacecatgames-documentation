cdc32f68ec41453143a3075dd5b283dd38010aa8
Ben <meowcat@meowcat.site>
2026-01-06 17:13:23 +0000

    it actually compiles now

--- spacecatgames-docs/docusaurus.config.js
+++ spacecatgames-docs/docusaurus.config.js
@@ -50,7 +50,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            '',
+            'https://github.com/neurontechofficial/spacecatgames-documentation',
         },
         blog: {
           showReadingTime: true,
