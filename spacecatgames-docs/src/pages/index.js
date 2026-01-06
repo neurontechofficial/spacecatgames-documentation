316b15cc65631177275d28b026561c337724d58a
Ben <meowcat@meowcat.site>
2026-01-06 17:11:35 +0000

    update index for new page!

--- spacecatgames-docs/src/pages/index.js
+++ spacecatgames-docs/src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Host Space Cat Games - 5min ⏱️
           </Link>
         </div>
       </div>
